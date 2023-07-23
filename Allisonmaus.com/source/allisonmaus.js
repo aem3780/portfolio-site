7e8a9263be2b65be21f51b6caee60ebca6a70128
Allie Maus <aem3780@rit.edu>
2023-07-23 11:43:59 -0400

    refactor

--- Allisonmaus.com/source/allisonmaus.js
+++
@@ -1,8 +0,0 @@
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-//toggle burger icon on and off for mobile
-burgerIcon.addEventListener('click', () => 
-{
-  navbarMenu.classList.toggle('is-active');
-});
--- Allisonmaus.com/source/carousel.js
+++
@@ -1,9 +0,0 @@
-bulmaCarousel.attach('#carousel-demo', {
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    infinite: true,
-    navigation: true,
-    navigationSwipe: true
-
-});
-//code cited on documentation page
