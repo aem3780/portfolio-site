0ff1a6e9ed2e7ebe4685136907aa183cddc10387
Allie Maus <aem3780@rit.edu>
2023-07-23 11:36:58 -0400

    fix

---
+++ source/carousel.js
@@ -0,0 +1,9 @@
+bulmaCarousel.attach('#carousel-demo', {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    infinite: true,
+    navigation: true,
+    navigationSwipe: true
+
+});
+//code cited on documentation page
